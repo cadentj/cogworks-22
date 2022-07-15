779934c7a93e4fa83160394780dbf1a8591433d9
Prerana <prerana.mg@gmail.com>
2022-07-15 08:50:05 -0700

    finished fanout func

--- fp_module/fingerprints/functions_b.py
+++ fp_module/fingerprints/functions_b.py
@@ -1,5 +1,39 @@
 import numpy as np
 
+def fanout(peaks: List[Tuple[int, int]], num_neighbors: int) -> List[Tuple[int, int, int]], List[int]: 
+    
+    fingerprints = []
+    time_sigs = []
 
-def multiply_and_sum(arr: np.ndarray, factor: float) -> float:
-    return float(np.sum(arr * factor))
+    for i in range(len(peaks) - num_neighbors):
+        peak[i][1] = f_i
+        
+        for j in range(num_neighbors):
+            peak[i + j][1] = f_j
+            delta_t = peak[i + j][0] - peak[i][0]
+            
+            fingerprints.append((f_i, f_j, delta_t))
+            time_sigs.append(peak[i][0])
+            
+    """
+    fanout funtion with chunks:
+    
+    fingerprints = []
+    time_sigs = []
+    
+    each_fp = [] 
+    
+    for i in range(len(peaks) - num_neighbors):
+        peak[i][1] = f_i
+        
+        for j in range(num_neighbors):
+            peak[i + j][1] = f_j
+            delta_t = peak[i + j][0] - peak[i][0]
+            each_fp.append((f_i, f_j, delta_t))
+            
+        fingerprints.append(each_fp)
+        each_fp = []
+        time_sigs.append(peak[i][0])
+    
+    """
+    return fingerprints, time_sigs
